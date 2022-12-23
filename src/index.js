c036da590abd286c8a39edb65954ab03a200f65b
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2022-12-22 21:18:50 -0500

    Implement basic playback from hard-coded file

--- src/index.js
+++ src/index.js
@@ -3,27 +3,131 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function Play() {
-  return (
-    <button>
-      Play
-    </button>
-  );
-}
+class Play extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-function Stop() {
-  return (
-    <button>
-      Stop
-    </button>
-  );
-}
+  handleClick(e) {
+    e.preventDefault();
+    this.props.onPlay()
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Play
+      </button>
+    );
+  }
+};
+
+class Stop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.onStop()
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Stop
+      </button>
+    );
+  }
+};
+
+class AudioPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.audioCtx = null;
+    this.srcBuf = null;
+    this.srcNode = null;
+    this.isPlaying = false;
+
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+  }
+
+  componentDidMount() {
+    this.audioCtx = new AudioContext();
+    fetch("Audiorezout-Resurgence.mp3")
+      .then((response) => response.arrayBuffer())
+      .then((downloadedBuffer) =>
+        this.audioCtx.decodeAudioData(downloadedBuffer)
+      )
+      .then((decodedBuffer) => {
+        this.srcBuf = decodedBuffer;
+      })
+      .catch((e) => {
+        console.error(`Error: ${e}`);
+      });
+  }
+
+  componentWillUnmount() {
+    if (this.audioCtx) {
+      this.audioCtx.close();
+    }
+  }
+
+  handlePlay() {
+    if (this.audioCtx.state === "suspended") {
+      this.audioCtx.resume().then(() => this.handlePlay());
+      return;
+    }
+    
+    if (this.isPlaying) {
+      return;
+    }
+
+    // Set up the AudioBufferSourceNode
+    this.srcNode = new AudioBufferSourceNode(this.audioCtx, {
+      buffer: this.srcBuf,
+      loop: true,
+    });
+
+    // Connect the nodes together
+    this.srcNode.connect(this.audioCtx.destination);
+
+    if (this.srcNode) {
+      this.srcNode.start(0);
+      this.isPlaying = true;
+    }
+  }
+
+  handleStop() {
+    if (!this.isPlaying) {
+      return;
+    }
+
+    if (this.srcNode) {
+      this.srcNode.stop(0);
+      this.isPlaying = false;
+      this.srcNode = null;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Play onPlay={this.handlePlay}/>
+        <Stop onStop={this.handleStop}/>
+      </div>
+    );
+  }
+};
 
 function App() {
   return (
     <div>
-      <Play/>
-      <Stop/>
+      <AudioPlayer/>
     </div>
   );
 }
