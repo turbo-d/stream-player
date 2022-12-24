552a9ba9501c6bdfc7069da357e9af035045f292
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2022-12-23 23:22:11 -0500

    Add time elapsed and time remaining counters

--- src/index.js
+++ src/index.js
@@ -50,18 +50,35 @@ class Stop extends React.Component {
   }
 };
 
+class PlaybackTime extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.playbackTime}</p>
+      </div>
+    );
+  }
+}
+
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
       isPaused: true,
+      timeElapsed: new Date(0),
     }
 
     this.audioCtx = null;
     this.srcBuf = null;
     this.srcNode = null;
     this.isPlaying = false;
+    this.timerID = null;
+    this.startTime = 0;
 
     this.handlePlay = this.handlePlay.bind(this);
     this.handleStop = this.handleStop.bind(this);
@@ -87,6 +104,14 @@ class AudioPlayer extends React.Component {
     if (this.audioCtx) {
       this.audioCtx.close();
     }
+  }
+
+  tick() {
+    const curTime = this.audioCtx.currentTime;
+
+    this.setState((state, props) => ({
+      timeElapsed: new Date(1000 * (curTime - this.startTime)),
+    }));
   }
 
   handlePlay() {
@@ -116,9 +141,14 @@ class AudioPlayer extends React.Component {
     if (this.srcNode) {
       this.srcNode.start(0);
       this.isPlaying = true;
+      this.startTime = this.audioCtx.currentTime;
       this.setState((state, props) => ({
         isPaused: false,
       }));
+      this.timerID = setInterval(
+        () => this.tick(),
+        100
+      );
     }
   }
 
@@ -132,7 +162,9 @@ class AudioPlayer extends React.Component {
       this.isPlaying = false;
       this.setState((state, props) => ({
         isPaused: true,
+        timeElapsed: new Date(0),
       }));
+      clearInterval(this.timerID);
       this.srcNode = null;
     }
   }
@@ -160,10 +192,27 @@ class AudioPlayer extends React.Component {
   }
 
   render() {
+    let timeElapsed = "-:--";
+    let timeRemaining = "-:--";
+
+    if (this.srcBuf) {
+      timeElapsed = this.state.timeElapsed.toISOString().substring(15, 19);
+
+      const trackLength = this.srcBuf.duration;
+      const minElapsed = this.state.timeElapsed.getMinutes();
+      const secElapsed = this.state.timeElapsed.getSeconds() + (60 * minElapsed);
+
+      timeRemaining = new Date(1000 * (trackLength - secElapsed)).toISOString().substring(15, 19);
+    }
+
+    timeRemaining = "-" + timeRemaining;
+
     return (
       <div>
         <Stop onStop={this.handleStop}/>
         <Play onPlay={this.handlePlay} onPause={this.handlePause} isPaused={this.state.isPaused}/>
+        <PlaybackTime playbackTime={timeElapsed}/>
+        <PlaybackTime playbackTime={timeRemaining}/>
       </div>
     );
   }
