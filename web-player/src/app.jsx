f0ab16fb9b8215cba18ce51889aa2a9d184af2bc
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2023-01-04 21:08:45 -0500

    Allow the user to select a track to be played

--- web-player/src/app.jsx
+++ web-player/src/app.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import AudioPlayer from './audioPlayer';
 import TrackList from './trackList';
 
+const StopReason = Object.freeze({
+  Undefined: "undefined",
+  End: "end",
+  Pause: "pause",
+  RTZ: "rtz",
+  Seek: "seek",
+})
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -10,21 +18,309 @@ class App extends React.Component {
       currentTrack: null,
     }
 
+    this.audioCtx = null;
+    this.srcBuf = null;
+    this.srcNode = null;
+    this.cursor = 0;
+    this.timerID = null;
+    this.lastTime = 0;
+    this.stopReason = StopReason.End;
+    this.isDragging = false;
+
     this.handleTrackSelect = this.handleTrackSelect.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handlePause = this.handlePause.bind(this);
+    this.handleRTZ = this.handleRTZ.bind(this);
+    this.handleOnBeforeSeek = this.handleOnBeforeSeek.bind(this);
+    this.handleOnSeek = this.handleOnSeek.bind(this);
+    this.handleOnAfterSeek = this.handleOnAfterSeek.bind(this);
   }
 
-  handleTrackSelect(track) {
+  componentDidMount() {
+    this.audioCtx = new AudioContext();
+
+    //const url = "http://localhost:8080/";
+    //fetch(url)
+    //  .then((response) => {
+    //    return response.arrayBuffer()
+    //  })
+    //  .then((downloadedBuffer) => {
+    //    return this.audioCtx.decodeAudioData(downloadedBuffer)
+    //  })
+    //  .then((decodedBuffer) => {
+    //    this.srcBuf = decodedBuffer;
+    //  })
+    //  .catch((e) => {
+    //    console.error(`Error: ${e}`);
+    //  });
+  }
+
+  componentWillUnmount() {
+    if (this.audioCtx) {
+      this.audioCtx.close();
+    }
+  }
+
+  tick() {
+    const curTime = this.audioCtx.currentTime;
+    const delta = curTime - this.lastTime;
+    this.lastTime = curTime;
+
+    this.cursor += delta;
+
+    if (!this.isDragging) {
+      let currentTrack = this.state.currentTrack;
+      currentTrack.seekLocation = Math.floor(this.cursor);
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+    }
+  }
+
+  sliceAudioBuffer(buf, start) {
+    if (!buf) {
+      return null;
+    }
+
+    if (start === 0) {
+      return buf;
+    }
+
+    let srcBufSlice = new AudioBuffer({
+      length: buf.length - start,
+      numberOfChannels: buf.numberOfChannels,
+      sampleRate: buf.sampleRate,
+    });
+
+    for (let chan = 0; chan < buf.numberOfChannels; chan++) {
+      let chanData = buf.getChannelData(chan);
+      let chanDataSubarray = chanData.subarray(start, chanData.length);
+      srcBufSlice.copyToChannel(chanDataSubarray, chan);
+    }
+
+    return srcBufSlice;
+  }
+
+  handleTrackSelect(trackMetaData) {
     //console.log("selected ", track);
+    // track {title, artist, seekLocation, duration, isPlaying}
+
+    if (this.state.currentTrack && this.state.currentTrack.isPlaying) {
+      this.srcNode.stop(0);
+    }
+
+    let track = {
+      title: trackMetaData.title,
+      artist: trackMetaData.artist,
+      duration: 0,
+      isPlaying: false,
+      seekLocation: 0
+    }
     this.setState((state, props) => ({
       currentTrack: track,
     }));
+
+
+    const url = "http://localhost:8080/playback/" + trackMetaData.id;
+    console.log(url);
+    fetch(url)
+      .then((response) => {
+        return response.arrayBuffer()
+      })
+      .then((downloadedBuffer) => {
+        return this.audioCtx.decodeAudioData(downloadedBuffer)
+      })
+      .then((decodedBuffer) => {
+        this.srcBuf = decodedBuffer;
+        let currentTrack = this.state.currentTrack;
+        currentTrack.duration = this.srcBuf.duration;
+        this.setState((state, props) => ({
+          currentTrack: currentTrack,
+        }));
+        this.handlePlay();
+      })
+      .catch((e) => {
+        console.error(`Error: ${e}`);
+      });
+  }
+
+  handlePlay() {
+    if (this.audioCtx.state === "suspended") {
+      this.audioCtx.resume().then(() => {
+        this.handlePlay();
+      });
+      return;
+    }
+
+    //if (this.state.currentTrack.isPlaying) {
+    //  return;
+    //}
+
+    let sampleFrame = Math.floor(this.cursor * this.audioCtx.sampleRate);
+    let buf = this.sliceAudioBuffer(this.srcBuf, sampleFrame);
+
+    // Set up the AudioBufferSourceNode
+    this.srcNode = new AudioBufferSourceNode(this.audioCtx, {
+      buffer: buf,
+    });
+
+    // Connect the nodes together
+    this.srcNode.connect(this.audioCtx.destination);
+
+    this.srcNode.addEventListener(
+      "ended",
+      () => {
+        this.srcNode = null;
+
+        switch (this.stopReason) {
+          case StopReason.End:
+            clearInterval(this.timerID);
+            this.cursor = 0;
+            let currentTrack = this.state.currentTrack;
+            currentTrack.isPlaying = false;
+            this.setState((state, props) => ({
+              currentTrack: currentTrack,
+            }));
+            if (!this.isDragging) {
+              let currentTrack = this.state.currentTrack;
+              currentTrack.seekLocation = Math.floor(this.cursor);
+              this.setState((state, props) => ({
+                currentTrack: currentTrack,
+              }));
+            }
+            break;
+          case StopReason.Pause:
+            break;
+          case StopReason.RTZ:
+            this.handlePlay();
+            break;
+          case StopReason.Seek:
+            this.handlePlay();
+            break;
+          default:
+            console.error("Unknown StopReason");
+        }
+
+        this.stopReason = StopReason.End;
+      },
+      false
+    )
+
+    if (this.srcNode) {
+      this.srcNode.start(0);
+      this.lastTime = this.audioCtx.currentTime;
+      let currentTrack = this.state.currentTrack;
+      currentTrack.isPlaying = true;
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+      this.timerID = setInterval(
+        () => this.tick(),
+        100
+      );
+    }
+  }
+
+  handlePause() {
+    if (!this.state.currentTrack.isPlaying) {
+      return;
+    }
+
+    if (this.srcNode) {
+      clearInterval(this.timerID);
+
+      let currentTrack = this.state.currentTrack;
+      currentTrack.isPlaying = false;
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+
+      this.stopReason = StopReason.Pause;
+      this.srcNode.stop(0);
+    }
+  }
+
+  handleRTZ() {
+    if (!this.state.currentTrack.isPlaying) {
+      this.cursor = 0;
+      let currentTrack = this.state.currentTrack;
+      currentTrack.seekLocation = Math.floor(this.cursor);
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+
+      return;
+    }
+
+    if (this.srcNode) {
+      clearInterval(this.timerID);
+
+      this.cursor = 0;
+      let currentTrack = this.state.currentTrack;
+      currentTrack.seekLocation = Math.floor(this.cursor);
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+
+      this.stopReason = StopReason.RTZ;
+      this.srcNode.stop(0);
+    }
+  }
+
+  handleOnBeforeSeek(value, thumbIndex) {
+    this.isDragging = true;
+  }
+
+  handleOnSeek(value, thumbIndex) {
+    let currentTrack = this.state.currentTrack;
+    currentTrack.seekLocation = value;
+    this.setState((state, props) => ({
+      currentTrack: currentTrack,
+    }));
+  }
+
+  handleOnAfterSeek(value, thumbIndex) {
+    this.isDragging = false;
+
+    if (!this.state.currentTrack.isPlaying) {
+      this.cursor = value;
+      let currentTrack = this.state.currentTrack;
+      currentTrack.seekLocation = Math.floor(this.cursor);
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+
+      return;
+    }
+
+    if (this.srcNode) {
+      clearInterval(this.timerID);
+
+      this.cursor = value;
+      let currentTrack = this.state.currentTrack;
+      currentTrack.seekLocation = Math.floor(this.cursor);
+      this.setState((state, props) => ({
+        currentTrack: currentTrack,
+      }));
+
+      this.stopReason = StopReason.Seek;
+      this.srcNode.stop(0);
+    }
   }
 
   render() {
     return (
       <div>
         <TrackList onTrackSelect={this.handleTrackSelect}/>
-        <AudioPlayer track={this.state.currentTrack}/>
+        <AudioPlayer
+          track={this.state.currentTrack}
+          onPlay={this.handlePlay}
+          onPause={this.handlePause}
+          onRTZ={this.handleRTZ}
+          onBeforeSeek={this.handleOnBeforeSeek}
+          onSeek={this.handleOnSeek}
+          onAfterSeek={this.handleOnAfterSeek}
+        />
       </div>
     );
   }
--- web-player/src/audioPlayer.jsx
+++ web-player/src/audioPlayer.jsx
@@ -7,31 +7,9 @@ import RTZ from './rtz';
 import TrackArtist from './trackArtist';
 import TrackTitle from './trackTitle';
 
-const StopReason = Object.freeze({
-  Undefined: "undefined",
-  End: "end",
-  Pause: "pause",
-  RTZ: "rtz",
-  Seek: "seek",
-})
-
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      srcBuf: null,
-      isPlaying: false,
-      seek: 0,
-    }
-
-    this.audioCtx = null;
-    this.srcNode = null;
-    this.cursor = 0;
-    this.timerID = null;
-    this.lastTime = 0;
-    this.stopReason = StopReason.End;
-    this.isDragging = false;
 
     this.handlePlay = this.handlePlay.bind(this);
     this.handlePause = this.handlePause.bind(this);
@@ -41,215 +19,28 @@ class AudioPlayer extends React.Component {
     this.handlePlaybackSliderAfterChange = this.handlePlaybackSliderAfterChange.bind(this);
   }
 
-  componentDidMount() {
-    this.audioCtx = new AudioContext();
-
-    const url = "http://localhost:8080/";
-    fetch(url)
-      .then((response) => {
-        return response.arrayBuffer()
-      })
-      .then((downloadedBuffer) => {
-        return this.audioCtx.decodeAudioData(downloadedBuffer)
-      })
-      .then((decodedBuffer) => {
-        this.setState((state, props) => ({
-          srcBuf: decodedBuffer,
-        }));
-      })
-      .catch((e) => {
-        console.error(`Error: ${e}`);
-      });
-  }
-
-  componentWillUnmount() {
-    if (this.audioCtx) {
-      this.audioCtx.close();
-    }
-  }
-
-  tick() {
-    const curTime = this.audioCtx.currentTime;
-    const delta = curTime - this.lastTime;
-    this.lastTime = curTime;
-
-    this.cursor += delta;
-
-    if (!this.isDragging) {
-      this.setState((state, props) => ({
-        seek: Math.floor(this.cursor),
-      }));
-    }
-  }
-
-  sliceAudioBuffer(buf, start) {
-    if (!buf) {
-      return null;
-    }
-
-    if (start === 0) {
-      return buf;
-    }
-
-    let srcBufSlice = new AudioBuffer({
-      length: buf.length - start,
-      numberOfChannels: buf.numberOfChannels,
-      sampleRate: buf.sampleRate,
-    });
-
-    for (let chan = 0; chan < buf.numberOfChannels; chan++) {
-      let chanData = buf.getChannelData(chan);
-      let chanDataSubarray = chanData.subarray(start, chanData.length);
-      srcBufSlice.copyToChannel(chanDataSubarray, chan);
-    }
-
-    return srcBufSlice;
-  }
-
   handlePlay() {
-    if (this.audioCtx.state === "suspended") {
-      this.audioCtx.resume().then(() => {
-        this.handlePlay();
-      });
-      return;
-    }
-
-    //if (this.state.isPlaying) {
-    //  return;
-    //}
-
-    let sampleFrame = Math.floor(this.cursor * this.audioCtx.sampleRate);
-    let buf = this.sliceAudioBuffer(this.state.srcBuf, sampleFrame);
-
-    // Set up the AudioBufferSourceNode
-    this.srcNode = new AudioBufferSourceNode(this.audioCtx, {
-      buffer: buf,
-    });
-
-    // Connect the nodes together
-    this.srcNode.connect(this.audioCtx.destination);
-
-    this.srcNode.addEventListener(
-      "ended",
-      () => {
-        this.srcNode = null;
-
-        switch (this.stopReason) {
-          case StopReason.End:
-            clearInterval(this.timerID);
-            this.cursor = 0;
-            this.setState((state, props) => ({
-              isPlaying: false,
-            }));
-            if (!this.isDragging) {
-              this.setState((state, props) => ({
-                seek: Math.floor(this.cursor),
-              }));
-            }
-            break;
-          case StopReason.Pause:
-            break;
-          case StopReason.RTZ:
-            this.handlePlay();
-            break;
-          case StopReason.Seek:
-            this.handlePlay();
-            break;
-          default:
-            console.error("Unknown StopReason");
-        }
-
-        this.stopReason = StopReason.End;
-      },
-      false
-    )
-
-    if (this.srcNode) {
-      this.srcNode.start(0);
-      this.lastTime = this.audioCtx.currentTime;
-      this.setState((state, props) => ({
-        isPlaying: true,
-      }));
-      this.timerID = setInterval(
-        () => this.tick(),
-        100
-      );
-    }
+    this.props.onPlay();
   }
 
   handlePause() {
-    if (!this.state.isPlaying) {
-      return;
-    }
-
-    if (this.srcNode) {
-      clearInterval(this.timerID);
-
-      this.setState((state, props) => ({
-        isPlaying: false,
-      }));
-
-      this.stopReason = StopReason.Pause;
-      this.srcNode.stop(0);
-    }
+    this.props.onPause();
   }
 
   handleRTZ() {
-    if (!this.state.isPlaying) {
-      this.cursor = 0;
-      this.setState((state, props) => ({
-        seek: Math.floor(this.cursor),
-      }));
-
-      return;
-    }
-
-    if (this.srcNode) {
-      clearInterval(this.timerID);
-
-      this.cursor = 0;
-      this.setState((state, props) => ({
-        seek: Math.floor(this.cursor),
-      }));
-
-      this.stopReason = StopReason.RTZ;
-      this.srcNode.stop(0);
-    }
+    this.props.onRTZ();
   }
 
   handlePlaybackSliderBeforeChange(value, thumbIndex) {
-    this.isDragging = true;
+    this.props.onBeforeSeek(value, thumbIndex);
   }
 
   handlePlaybackSliderChange(value, thumbIndex) {
-    this.setState((state, props) => ({
-      seek: value,
-    }));
+    this.props.onSeek(value, thumbIndex);
   }
 
   handlePlaybackSliderAfterChange(value, thumbIndex) {
-    this.isDragging = false;
-
-    if (!this.state.isPlaying) {
-      this.cursor = value;
-      this.setState((state, props) => ({
-        seek: Math.floor(this.cursor),
-      }));
-
-      return;
-    }
-
-    if (this.srcNode) {
-      clearInterval(this.timerID);
-
-      this.cursor = value;
-      this.setState((state, props) => ({
-        seek: Math.floor(this.cursor),
-      }));
-
-      this.stopReason = StopReason.Seek;
-      this.srcNode.stop(0);
-    }
+    this.props.onAfterSeek(value, thumbIndex);
   }
 
   render() {
@@ -262,26 +53,29 @@ class AudioPlayer extends React.Component {
 
     let trackTitle = "";
     let trackArtist = "";
+
+    let isPlaying = false;
+
     if (this.props.track) {
       trackTitle = this.props.track.title;
       trackArtist = this.props.track.artist;
-    }
 
-    if (this.state.srcBuf) {
-      const trackLength = this.state.srcBuf.duration;
+      const trackLength = this.props.track.duration;
 
-      seekLocation = this.state.seek;
+      seekLocation = this.props.track.seekLocation;
       maxSlider = Math.floor(trackLength);
       disableThumb = false;
 
       timeElapsed = new Date(1000 * (seekLocation)).toISOString().substring(15, 19);
       duration = new Date(1000 * (trackLength)).toISOString().substring(15, 19);
+
+      isPlaying = this.props.track.isPlaying;
     }
 
     return (
       <div>
         <RTZ onRTZ={this.handleRTZ} />
-        <Play onPlay={this.handlePlay} onPause={this.handlePause} isPlaying={this.state.isPlaying} />
+        <Play onPlay={this.handlePlay} onPause={this.handlePause} isPlaying={isPlaying} />
         <PlaybackTime playbackTime={timeElapsed} />
         <PlaybackTime playbackTime={duration} />
         <PlaybackSlider
