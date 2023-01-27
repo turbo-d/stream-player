b66ce40ba8aa2d77d3ab5c76607579eb3d8a82a2
Dale Luginbuhl <dale.luginbuhl@gmail.com>
2023-01-27 14:09:02 -0500

    Turn off React StrictMode

--- web-player/src/index.js
+++ web-player/src/index.js
@@ -6,9 +6,10 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
